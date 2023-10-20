1b9eaea857ca7e1f225840d492102a72e39ae5a7
Victor Moreno <>
2023-10-20 22:33:12 +0200

    refactor: monorepo

--- README.md
+++
@@ -1,2 +0,0 @@
-# ts-katas
-Katas in Typescript
---
+++ packages/gilded-rose/test/mocha/gilded-rose.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { GildedRose, Item } from "../../app/gilded-rose";
+
+describe('Gilded Rose', () => {
+  it('should foo', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal('fixme');
+  });
+});
