e80317da9fe3b7834e152139a1063539c2dc5fbe
Victor Moreno <>
2023-10-21 14:20:51 +0200

    refactor: BankMother test helper

--- packages/bank/test/Bank.test.ts
+++ packages/bank/test/Bank.test.ts
@@ -3,22 +3,32 @@ import { Bank } from "../src/Bank";
 
 describe("Bank", () => {
   it("should be able to deposit money", () => {
-    const bank = new Bank();
+    const bank = BankMother.anEmptyBank();
     bank.deposit(1000);
     expect(bank.balance).eq(1000);
   });
 
   it("should be able to withdraw money", () => {
-    const bank = new Bank();
-    bank.deposit(1000);
+    const bank = BankMother.aBankWithABalanceOf(1000);
     bank.withdraw(500);
     expect(bank.balance).eq(500);
   });
 
 
   it("should not be able to withdraw more than the balance", () => {
-    const bank = new Bank();
-    bank.deposit(1000);
+    const bank = BankMother.aBankWithABalanceOf(1000);
     expect(() => bank.withdraw(5000)).throw("Insufficient funds");
   });
 });
+
+class BankMother {
+  static anEmptyBank(): Bank {
+    return new Bank();
+  }
+
+  static aBankWithABalanceOf(balance: number): Bank {
+    const bank = new Bank();
+    bank.deposit(balance);
+    return bank;
+  }
+}
