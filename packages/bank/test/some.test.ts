f192e945cb927f43a4fa696673f96ff109658967
Victor Moreno <>
2023-10-21 14:09:52 +0200

    test: add recomended acceptance test

--- packages/bank/test/some.test.ts
+++ packages/bank/test/some.test.ts
@@ -1,7 +1,66 @@
 import { expect } from "chai";
+import { after, before } from "mocha";
+import sinon, { SinonSpy, spy } from "sinon";
 
-describe('dummy test', () => {
-  it('should fail', () => {
-    expect(true).to.be.false;
+describe('bank acceptance tests', () => {
+  let bank: AccountService;
+  before(() => {
+    bank = new Bank();
+  });
+
+  describe('Given a client makes a deposit of 1000 on 10-01-2012', () => {
+    before(() => {
+      bank.deposit(1000);
+    });
+    describe('And a deposit of 2000 on 13-01-2012', () => {
+      before(() => {
+        bank.deposit(2000);
+      });
+      describe('When they make a withdrawal of And a withdrawal of 500 on 14-01-2012', () => {
+        before(() => {
+          bank.withdraw(500);
+        });
+        describe('When they print their bank statement', () => {
+          after(() => {
+            sinon.restore();
+          });
+          it('Then they would see:', () => {
+            const expectedStatement = `
+            Date       || Amount || Balance
+            14/01/2012 || -500   || 2500
+            13/01/2012 || 2000   || 3000
+            10/01/2012 || 1000   || 1000
+            `;
+
+            bank.printStatement();
+            const calledWith = calledWithArgs(bank, expectedStatement);
+            expect(calledWith).equal(true);
+          });
+        });
+      });
+    });
   });
 });
+
+function calledWithArgs(bank: AccountService, args: string): boolean {
+  return false;
+}
+
+
+interface AccountService {
+  deposit(amount: number): void;
+  withdraw(amount: number): void;
+  printStatement(): void;
+}
+
+class Bank implements AccountService {
+  deposit(amount: number): void {
+    throw new Error("Method not implemented.");
+  }
+  withdraw(amount: number): void {
+    throw new Error("Method not implemented.");
+  }
+  printStatement(): void {
+    throw new Error("Method not implemented.");
+  }
+}
