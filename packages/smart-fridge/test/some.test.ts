6ccdee53160c7d037915328e7f3bd34f6cfe15b9
Victor Moreno <>
2023-10-24 16:37:37 +0200

    test: rename suite and test

--- packages/smart-fridge/test/some.test.ts
+++ packages/smart-fridge/test/some.test.ts
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 import { Fridge } from "../src/Frigde";
 
-describe('dummy test', () => {
-  it('Fridge must hold items', () => {
+describe(`${Fridge.name}`, () => {
+  it('should must hold items', () => {
     const fridge = new Fridge();
     expect(fridge.count).equal(0);
   });
