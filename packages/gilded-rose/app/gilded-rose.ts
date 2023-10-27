dd84850ee7e3abc512ffcbfdea002534e54c3bad
Ángel Siendones Sillero <culoextremo@gmail.com>
2023-10-27 12:46:49 +0200

    refactor: extract isFresh method

--- packages/gilded-rose/app/gilded-rose.ts
+++ packages/gilded-rose/app/gilded-rose.ts
@@ -11,14 +11,14 @@ export class GildedRose {
     for (let i = 0; i < this.items.length; i++) {
       if (!this.equalsItemName(i, 'Aged Brie') && !this.equalsItemName(i, 'Backstage passes to a TAFKAL80ETC concert')) {
         if (this.isFresh(i)) {
-          if (!this.equalsItemName(i, 'Sulfuras, Hand of Ragnaros')) {
+          if (!this.isLegendary(i)) {
             this.decreaseItemQuality(i);
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.increaseItemQuality(i);
-          if (this.equalsItemName(i, 'Backstage passes to a TAFKAL80ETC concert')) {
+          if (this.equalsItemName(i, 'Backstage passes to a TAFKAL80ETC concert') ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.increaseItemQuality(i);
@@ -32,14 +32,14 @@ export class GildedRose {
           }
         }
       }
-      if (!this.equalsItemName(i, 'Sulfuras, Hand of Ragnaros')) {
+      if (!this.isLegendary(i)) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (!this.equalsItemName(i, 'Aged Brie')) {
+        if (this.items[i].name != 'Aged Brie') {
           if (!this.equalsItemName(i, 'Backstage passes to a TAFKAL80ETC concert')) {
             if (this.isFresh(i)) {
-              if (!this.equalsItemName(i, 'Sulfuras, Hand of Ragnaros')) {
+              if (!this.isLegendary(i)) {
                 this.decreaseItemQuality(i);
               }
             }
@@ -61,6 +61,10 @@ export class GildedRose {
     return this.items[i].quality > 0;
   }
 
+  private isLegendary(i: number) {
+    return this.equalsItemName(i, 'Sulfuras, Hand of Ragnaros');
+  }
+
   private equalsItemName(i: number, name: string) {
     return this.items[i].name == name;
   }
@@ -71,10 +75,6 @@ export class GildedRose {
 
   private increaseItemQuality(i: number) {
     this.items[i].quality = this.items[i].quality + 1;
-  }
-
-  private isLegendary(i: number) {
-    return this.equalsItemName(i, 'Sulfuras, Hand of Ragnaros');
   }
 
 
