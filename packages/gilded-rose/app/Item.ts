ee6cf2b93bf7e8b5c97473c763a6c55e47ea504a
Victor Moreno <>
2023-10-28 21:18:42 +0200

    decreaseQuality with delta parameter

--- packages/gilded-rose/app/Item.ts
+++ packages/gilded-rose/app/Item.ts
@@ -74,19 +74,19 @@ export class Item {
 
   }
 
-  private decreaseQuality() {
+  private decreaseQuality(delta: number = 1) {
     if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
       this.expire();
     }
     else {
-      this.decreaseQualityByOne();
+      this.decreaseQualityBy(delta);
 
     }
   }
 
-  private decreaseQualityByOne() {
+  private decreaseQualityBy(delta: number = 1) {
     if (this.quality > 0) {
-      this.quality -= 1;
+      this.quality -= delta;
     }
   }
 
