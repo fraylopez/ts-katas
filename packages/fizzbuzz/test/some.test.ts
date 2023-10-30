b1a8926965c14b6f233fccfa1fc663332c716f76
Victor Moreno <>
2023-10-30 09:00:20 +0100

    feat: 1

--- packages/fizzbuzz/test/some.test.ts
+++ packages/fizzbuzz/test/some.test.ts
@@ -1,93 +1,14 @@
 import { expect, should } from "chai";
-import { GameOfLife } from "../src/GameOfLife";
 
 should();
-describe(`${GameOfLife.name}`, () => {
-  it('should create a game', () => {
-    GameOfLife.empty();
+describe("FizzBuzz", () => {
+  it('should output the fizzbuzz sequence', () => {
+    expect(FizzBuzz(1)).to.equal('1');
   });
-
-  it('empty does not change', () => {
-    GameOfLife.empty()
-      .nextGen()
-      .should.eql(GameOfLife.empty());
-  });
-
-  it('single cell dies', () => {
-    const board = [
-      [0, 0, 0],
-      [0, 1, 0],
-      [0, 0, 0],
-    ];
-    GameOfLife.from(board)
-      .nextGen()
-      .should.eql(GameOfLife.empty());
-  });
-
-  it('change', () => {
-    const board = [
-      [0, 0, 0],
-      [0, 1, 0],
-      [0, 0, 0],
-    ];
-    GameOfLife.from(board)
-      .nextGen()
-      .should.not.eql(GameOfLife.from(board));
-  });
-
-  it('underpopulation with neighbours', () => {
-    const board = [
-      [1, 1],
-    ];
-    GameOfLife.from(board)
-      .nextGen()
-      .should.eql(GameOfLife.empty());
-  });
-
-  it('overpopulation with neighbours all die', () => {
-    const board = [
-      [1, 1, 1],
-      [1, 1, 1],
-      [1, 1, 1],
-    ];
-    GameOfLife.from(board)
-      .nextGen()
-      .should.eql(GameOfLife.empty());
-  });
-
-  it('overpopulation with neighbours', () => {
-    const board = [
-      [1, 0, 1],
-      [0, 1, 0],
-      [1, 0, 1],
-    ];
-    GameOfLife.from(board)
-      .nextGen()
-      .should.eql(GameOfLife.empty());
-  });
-
-  it('reproduction', () => {
-    const board = [
-      [1, 0, 1],
-      [0, 0, 0],
-      [0, 0, 1],
-    ];
-    GameOfLife.from(board)
-      .nextGen()
-      .should.eql(GameOfLife.from([
-        [0, 0, 0],
-        [0, 1, 0],
-        [0, 0, 0],
-      ]));
-
-  });
-
-
 
 });
 
-// underpopulation
-// overpopulation
-// reproduction
-// surviving
-// cells out of bounds die
+
+function FizzBuzz(n: number): string {
+  return '1';
+}
