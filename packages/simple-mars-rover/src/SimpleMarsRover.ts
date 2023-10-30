0e69aa6397ad3c769901bb65353e615e4febb70b
Victor Moreno <>
2023-10-30 10:23:01 +0100

    refactor: move to new file

---
+++ packages/simple-mars-rover/src/SimpleMarsRover.ts
@@ -0,0 +1,44 @@
+export class SimpleMarsRover {
+  private x: number;
+  private y: number;
+  private orientation: string;
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.orientation = 'N';
+  }
+  run(commands: string): string {
+    if (commands === 'R') {
+      return '0:0:E';
+    }
+    if (commands.indexOf('L') > -1) {
+      return `${this.x}:${this.y}:${this.turnLeftTimes(this.findNumberOfCommandsOfType(commands, 'L'))}`;
+    }
+    if (commands.indexOf('M') > -1) {
+      return `0:${commands.length}:N`;
+    }
+    return '0:0:N';
+  }
+
+  private turnLeftTimes(times: number) {
+    for (let i = 0; i < times; i++) {
+      this.orientation = this.turnLeft();
+    }
+    return this.orientation;
+  }
+
+  private turnLeft() {
+    const directionsLeft = ['N', 'W', 'S', 'E'];
+    const currentDirectionIndex = directionsLeft.indexOf(this.orientation);
+    if (currentDirectionIndex === 0) {
+      return 'W';
+    }
+    return `${directionsLeft[currentDirectionIndex + 1]}`;
+  }
+
+
+
+  private findNumberOfCommandsOfType(commands: string, type: string) {
+    return Array.from(commands).filter(command => command === type).length;
+  }
+}
--- packages/simple-mars-rover/test/some.test.ts
+++ packages/simple-mars-rover/test/some.test.ts
@@ -1,4 +1,5 @@
 import { expect, should } from "chai";
+import { SimpleMarsRover } from "../src/SimpleMarsRover";
 
 should();
 describe("SimpleMarsRover", () => {
@@ -35,56 +36,3 @@ describe("SimpleMarsRover", () => {
 });
 
 
-class SimpleMarsRover {
-  private x: number;
-  private y: number;
-  private orientation: string;
-  constructor() {
-    this.x = 0;
-    this.y = 0;
-    this.orientation = 'N';
-  }
-  run(commands: string): string {
-    if (commands === 'R') {
-      return '0:0:E';
-    }
-    if (commands.indexOf('L') > -1) {
-      return `${this.x}:${this.y}:${this.turnLeftTimes(this.findNumberOfCommandsOfType(commands, 'L'))}`;
-    }
-    if (commands.indexOf('M') > -1) {
-      return `0:${commands.length}:N`;
-    }
-    return '0:0:N';
-  }
-
-  private turnLeftTimes(times: number) {
-    for (let i = 0; i < times; i++) {
-      this.orientation = this.turnLeft();
-    }
-    return this.orientation;
-  }
-
-  private turnLeft() {
-    const directionsLeft = ['N', 'W', 'S', 'E'];
-    const currentDirectionIndex = directionsLeft.indexOf(this.orientation);
-    if (currentDirectionIndex === 0) {
-      return 'W';
-    }
-    return `${directionsLeft[currentDirectionIndex + 1]}`;
-  }
-
-
-
-  private findNumberOfCommandsOfType(commands: string, type: string) {
-    return Array.from(commands).filter(command => command === type).length;
-  }
-}
-
-
-// starts at 0,0 facing north
-// input sequence of commands
-//// L = turn left
-//// R = turn right
-//// M = move forward
-// wraps around edges of grid
-// return final position and direction facing
