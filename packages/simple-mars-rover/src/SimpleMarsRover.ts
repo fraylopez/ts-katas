c081baa63bbed071ad71bef1cac57bbe801aa8e9
Victor Moreno <>
2023-10-30 10:35:05 +0100

    refactor: generalize concrete code

--- packages/simple-mars-rover/src/SimpleMarsRover.ts
+++ packages/simple-mars-rover/src/SimpleMarsRover.ts
@@ -8,46 +8,34 @@ export class SimpleMarsRover {
     this.orientation = 'N';
   }
   run(commands: string): string {
-    if (commands.indexOf('R') > -1) {
-      this.turnRightTimes(this.findNumberOfCommandsOfType(commands, 'R'));
-    }
-    if (commands.indexOf('L') > -1) {
-      this.turnLeftTimes(this.findNumberOfCommandsOfType(commands, 'L'));
-    }
-    if (commands.indexOf('M') > -1) {
-      this.y = this.y + this.findNumberOfCommandsOfType(commands, 'M');
+    for (let i = 0; i < commands.length; i++) {
+      this.applyCommand(commands[i]);
     }
     return `${this.x}:${this.y}:${this.orientation}`;
+
   }
 
-  private turnLeftTimes(times: number) {
-    for (let i = 0; i < times; i++) {
-      this.orientation = this.turnLeft();
+  private applyCommand(command: string) {
+    if (command === 'R') {
+      this.turnRight();
     }
-    return this.orientation;
-  }
-
-  private turnRightTimes(times: number) {
-    for (let i = 0; i < times; i++) {
-      this.orientation = this.turnRight();
+    if (command === 'L') {
+      this.turnLeft();
     }
-    return this.orientation;
+    if (command === 'M') {
+      this.y = this.y + 1;
+    }
   }
-
   private turnLeft() {
-    return this.turn(['N', 'W', 'S', 'E']);
+    this.turn(['N', 'W', 'S', 'E']);
   }
 
   private turnRight() {
-    return this.turn(['N', 'E', 'S', 'W']);
+    this.turn(['N', 'E', 'S', 'W']);
   }
 
   private turn(directions: string[]) {
     const currentDirectionIndex = directions.indexOf(this.orientation);
-    return `${directions[currentDirectionIndex + 1]}`;
-  }
-
-  private findNumberOfCommandsOfType(commands: string, type: string) {
-    return Array.from(commands).filter(command => command === type).length;
+    this.orientation = `${directions[currentDirectionIndex + 1]}`;
   }
 }
