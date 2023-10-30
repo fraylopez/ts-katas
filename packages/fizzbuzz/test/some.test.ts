f633b7605c062def0d86ebe3c4a9ed43bda8d17b
Victor Moreno <>
2023-10-30 09:04:16 +0100

    feat: 2

--- packages/fizzbuzz/test/some.test.ts
+++ packages/fizzbuzz/test/some.test.ts
@@ -2,13 +2,30 @@ import { expect, should } from "chai";
 
 should();
 describe("FizzBuzz", () => {
-  it('should output the fizzbuzz sequence', () => {
-    expect(FizzBuzz(1)).to.equal('1');
+  [
+    {
+      fizzBuzzOf: 1,
+      expected: '1'
+    },
+    {
+      fizzBuzzOf: 2,
+      expected: '1, 2'
+    },
+  ].forEach((testCase) => {
+    it(`should output the fizzbuzz sequence for ${testCase.fizzBuzzOf}`, () => {
+      expect(FizzBuzz(testCase.fizzBuzzOf)).to.equal(testCase.expected);
+    });
   });
+
+
 
 });
 
 
 function FizzBuzz(n: number): string {
-  return '1';
+  const result: number[] = [1];
+  if (n === 2) {
+    result.push(n);
+  }
+  return result.join(', ');
 }
