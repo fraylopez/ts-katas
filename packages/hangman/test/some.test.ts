7acced15d1589dd5c9ed3105011096e476317f6d
Victor Moreno <victor.moreno@cabify.com>
2024-05-24 18:01:17 +0200

    feat: get word length

--- packages/hangman/test/some.test.ts
+++ packages/hangman/test/some.test.ts
@@ -2,8 +2,22 @@ import { expect, should } from "chai";
 should();
 describe("Hangman", () => {
 
-  it("", () => {
+  it("should have a word of length 3", () => {
+    new Hangman().wordLength().should.be.equal(3);
+  });
+
+  it("should have a word of length 4", () => {
+    new Hangman("moon").wordLength().should.be.equal(4);
   });
 
 
 });
+
+
+class Hangman {
+  constructor(private word: string = "cat") {
+  }
+  wordLength() {
+    return this.word.length;
+  }
+}
