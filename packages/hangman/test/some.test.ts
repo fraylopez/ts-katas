bb4a58df0192031c8c639a1c75f0610cc4816ccb
Victor Moreno <victor.moreno@cabify.com>
2024-05-24 18:10:49 +0200

    feat: lives

--- packages/hangman/test/some.test.ts
+++ packages/hangman/test/some.test.ts
@@ -10,14 +10,37 @@ describe("Hangman", () => {
     new Hangman("moon").wordLength().should.be.equal(4);
   });
 
+  it("should guess a letter", () => {
+    const hangman = new Hangman("dad");
+    hangman.guess("a");
+    hangman.getLives().should.be.equal(10);
+  });
 
+  it("should fail a guess", () => {
+    const hangman = new Hangman("dad");
+    hangman.guess("b");
+    hangman.getLives().should.be.equal(9);
+  });
 });
 
 
 class Hangman {
-  constructor(private word: string = "cat") {
+  private word: string;
+  private lives: number;
+  constructor(word: string = "dad") {
+    this.word = word;
+    this.lives = 10;
   }
   wordLength() {
     return this.word.length;
   }
-}
+  guess(letter: string) {
+    if (!this.word.includes(letter)) {
+      this.lives--;
+    }
+  }
+  getLives() {
+    return this.lives;
+  }
+
+};
