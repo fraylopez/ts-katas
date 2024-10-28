4f20fe5e3df79f15b6d0a3f6a749d1691060085a
Victor Moreno <victor.moreno@cabify.com>
2024-10-28 19:15:27 +0100

    test: logs error

--- packages/birthday-greetings/test/some.test.ts
+++ packages/birthday-greetings/test/some.test.ts
@@ -1,19 +1,24 @@
 import * as sinon from "sinon";
 import { assert } from "sinon";
-import { BirthdayService } from "../src/birthdayService";
 import { Probe } from "../src/Probe";
 import * as  Mail from "nodemailer/lib/mailer";
+import { expect } from "chai";
+import { EmailNotSentError } from "../src/EmailNotSentError";
+import { BirthdayService } from "../src/BirthdayService";
 
 let sandbox: sinon.SinonSandbox;
 let loggerStub: sinon.SinonStub;
 
 before(() => {
   sandbox = sinon.createSandbox();
+});
+
+beforeEach(() => {
   loggerStub = sandbox.stub(Probe, "log");
 });
 
 afterEach(() => {
-  sandbox.reset();
+  sandbox.restore();
 });
 
 it.skip("should be green when the refactor is done", () => {
@@ -38,18 +43,30 @@ it("should load the txt file", () => {
   );
 });
 
-it("should attempt to send an email", () => {
-  BirthdayService.main("some args");
+it("should attempt to send an email", async () => {
+  await BirthdayService.main("some args");
   assert.calledWith(loggerStub, "Sending email");
 });
 
 it("should send the email", async () => {
-  mockMailService();
+  mockMailSent();
   await BirthdayService.main("some args");
   assert.calledWith(loggerStub, "Email sent");
 });
 
-function mockMailService() {
-  sandbox.stub(Mail.prototype, "sendMail").resolves();
+it("should log an error if the email is not sent", async () => {
+  mockMailNotSent();
+  await BirthdayService.main("some args");
+  assert.calledWith(loggerStub, "Error", sinon.match.instanceOf(EmailNotSentError));
+});
+
+function mockMailSent() {
+  sandbox.stub(Mail.prototype, "sendMail")
+    .resolves();
+}
+
+function mockMailNotSent() {
+  sandbox.stub(Mail.prototype, "sendMail")
+    .rejects(new Error("some error"));
 }
 
